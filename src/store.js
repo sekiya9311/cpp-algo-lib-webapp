a9fa23424c293472cf4e830b0852b10ddcbfe0f4
sekiya9311 <sekiya9311@gmail.com>
2018-05-25 01:06:13 +0900

    github api 使ったデータ取得書いてみた(動くとは言ってない)

--- src/store.js
+++ src/store.js
@@ -12,28 +12,44 @@ module.exports = new Vue({
   },
   methods: {
     getApi(path, func) {
-      axios.get(path)
-      .then(res => {
+      axios.get(path).then((res) => {
         func(res.data)
       })
     },
     getTree() {
       this.getApi(RepositoryContents, (data) => {
-
+        data.forEach((item) => {
+          this.makeTree(item, (node) => {
+            this.tree.push(node)
+          })
+        })
       })
     },
-    makeTree(node) {
-      // return tree with node as root
+    makeTree(node, func) {
+      let retTree = {
+        name: node.name,
+        content: '',
+        link: url,
+        nodes: []
+      }
       if (node.type === 'file') {
         if (node.name.endsWith('cpp')) {
           // cppファイル以外は無視
           this.getApi(node.download_url, (data) => {
-            Object.defineProperties(node, 'source', data)
+            retTree.content = data
+            func(retTree)
           })
         }
       } else if (node.type === 'dir') {
+        this.getApi(node.url, (data) => {
+          data.forEach((item) => {
+            this.makeTree(item, (child) => {
+              retTree.nodes.push(child)
+            })
+          })
+          func(retTree)
+        })
       }
-      return node
     }
   }
 })
