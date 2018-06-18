0440a76529af9dcc014a546d803419efca8c9ad5
sekiya9311 <sekiya9311@gmail.com>
2018-06-19 01:00:09 +0900

    Github API での取得をGAS経由に変更

---
+++ gas/main.gs
@@ -0,0 +1,44 @@
+function doGet() {
+  const RepositoryAddress = 'https://api.github.com/repos/sekiya9311/CplusplusAlgorithmLibrary/contents';
+  const AccessToken = '見せられないよ!';
+  const url = RepositoryAddress + '/?access_token=' + AccessToken;
+  var res = JSON.parse(UrlFetchApp.fetch(url).getContentText());
+  var tree = [];
+  
+  for (var i = 0; i < res.length; i++) {
+    var node = res[i];
+    var ret = makeTree(node, AccessToken);
+    if (ret) {
+      tree.push(ret);
+    }
+  }
+  var out = ContentService.createTextOutput();
+  out.setContent((JSON.stringify(tree)));
+  out.setMimeType(ContentService.MimeType.JSON);
+  return out;
+}
+
+function makeTree(data, AccessToken) {
+  var node = {
+    name: data.name,
+    content: '',
+    link: data.url,
+    nodes: []
+  };
+  if (data.type === 'dir') {
+    const url = data.url + '&' + 'access_token=' + AccessToken;
+    var res = JSON.parse(UrlFetchApp.fetch(url).getContentText());
+    for (var i = 0; i < res.length; i++) {
+      var nxtNode = res[i];
+      var ret = makeTree(nxtNode, AccessToken);
+      if (ret) {
+        node.nodes.push(ret);
+      }
+    }
+  } else if (data.type === 'file' && data.name.lastIndexOf('.cpp') === data.name.length - 4) {
+    node.content = UrlFetchApp.fetch(data.download_url).getContentText();
+  } else {
+    return null;
+  }
+  return node;
+}
--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app" class="el-container is-vertical">
     <div id="header" class="el-header">
       header dayo!
-      <input-token-dialog @send-token="getRepository" />
     </div>
     <div class="el-container">
       <div id="sidebar" class="el-aside">
@@ -28,10 +27,7 @@ export default {
   name: 'App',
   data () {
     return {
-      Token: '',
       tree: [],
-      RepositoryContents: 'https://api.github.com/repos/sekiya9311/CplusplusAlgorithmLibrary/contents',
-      AccessToken: '',
       sourceData: {
         name: 'main',
         content: 'int main() {}'
@@ -40,77 +36,34 @@ export default {
     }
   },
   components: {
-    InputTokenDialog,
     RepositoryTree,
     SourcePage,
     SearchResults
   },
   methods: {
-    getRepository (tkn) {
-      console.log(this)
-      this.Token = tkn
-      console.log('token: ' + this.Token)
-      // TODO: get repository tree
-      //       send RepositoryTree and SearchResult
-      this.getTree(this.Token)
-    },
-    getApi (path, func) {
-      axios.get(path).then((res) => {
-        func(res.data)
-      }).catch((err) => {
-        console.log(err)
-        console.log('path: ' + path)
-      })
-    },
-    getTree (token) {
-      if (token) {
-        this.AccessToken = 'access_token=' + token
-      }
-      this.getApi(this.RepositoryContents + '/?' + this.AccessToken, (data) => {
-        data.forEach((item) => {
-          this.makeTree(item, (node) => {
-            this.tree.push(node)
-          })
-        })
-      })
-    },
-    makeTree (node, func) {
-      let retTree = {
-        name: node.name,
-        content: '',
-        link: node.url,
-        nodes: []
-      }
-      if (node.type === 'file') {
-        if (node.name.endsWith('cpp')) {
-          // cppファイル以外は無視
-          this.getApi(node.download_url, (data) => {
-            retTree.content = data
-            func(retTree)
-          })
-        }
-      } else if (node.type === 'dir') {
-        this.getApi(node.url + '&' + this.AccessToken, (data) => {
-          data.forEach((item) => {
-            this.makeTree(item, (child) => {
-              retTree.nodes.push(child)
-            })
-          })
-          func(retTree)
-        })
-      }
-    },
     setSourceCode (data) {
       this.sourceData = data
       this.sourcePageVisible = true
-      const foo = setInterval(() => {
-        if (document.getElementsByClassName('el-dialog__wrapper')[1].scrollTop) {
-          document.getElementsByClassName('el-dialog__wrapper')[1].scrollTop = 0
+      const id = setInterval(() => {
+        let dlgDom = document.getElementsByClassName('el-dialog__wrapper')
+        if (dlgDom[0].scrollTop) {
+          dlgDom[0].scrollTop = 0
+          clearInterval(id)
         }
-        clearInterval(foo)
-      }, 10)
-      console.log(this.sourceData.content)
+      }, 10);
     }
+  },
+  mounted: function() {
+    const url = 'https://script.google.com/macros/s/AKfycbyJVBH-DZ-3d_rhj8tdVgvOFGqFYt43F9WA7B-8E-AlGOD5B6Y/exec'
+    axios.get(url)
+         .then((res) => this.tree = res.data)
+         .catch((err) => {
+           console.log(err)
+           this.sourceData = {
+             name: 'Error!',
+             content: 'Error!'
+           }
+         })
   }
 }
 </script>
