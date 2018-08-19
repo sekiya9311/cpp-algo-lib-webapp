069769348c45aa7111079d975ad30222cef67629
sekiya9311 <sekiya9311@gmail.com>
2018-08-19 10:01:09 +0900

    検索結果にコピーボタン追加

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -3,6 +3,9 @@
     <div class="title">
       {{ data.name }}
     </div>
+    <el-button v-on:click="sourceCopy" v-bind:disabled="!this.data.content">
+        Copy
+      </el-button>
     <div class="source">
       <pre v-highlightjs="data.content">
         <code class="cpp" />
@@ -16,7 +19,35 @@ export default {
   data () {
     return {}
   },
-  props: ['data']
+  props: ['data'],
+  methods: {
+    sourceCopy () {
+      window.getSelection().removeAllRanges()
+
+      const fooTextArea = document.createElement('textarea')
+      fooTextArea.textContent = this.data.content
+
+      document.body.appendChild(fooTextArea)
+
+      fooTextArea.select()
+
+      const isFuntionSupported = document.execCommand('copy')
+
+      document.body.removeChild(fooTextArea)
+
+      if (isFuntionSupported) {
+        this.$message({
+          message: 'Copied!',
+          type: 'success'
+        })
+      } else {
+        this.$message({
+          message: 'not supported this function ...',
+          type: 'warning'
+        })
+      }
+    }
+  }
 }
 </script>
 
