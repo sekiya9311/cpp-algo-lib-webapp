3b0c05ad4f435af9a11afd22ad5929ad1ff34e24
sekiya9311 <sekiya9311@gmail.com>
2019-08-28 23:48:15 +0900

    Add IconLink component

---
+++ src/components/IconLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <a :href="this.url" :style="{fontSize: fontSize}" target="__blank">
+    <font-awesome-icon :icon="this.icon" />
+    <small>
+      {{this.detail}}
+    </small>
+  </a>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { IconDefinition } from '@fortawesome/fontawesome-common-types'
+
+export default Vue.extend({
+  props: {
+    icon: Object as PropType<IconDefinition>,
+    url: String,
+    detail: String,
+    fontSize: String
+  },
+  components: {
+    FontAwesomeIcon
+  }
+})
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
