8f5d1034ec3d7f46dc039829327df688984c80fa
sekiya9311 <sekiya9311@gmail.com>
2019-09-23 00:03:33 +0900

    Add DisplaySourceCodes

---
+++ src/components/DisplaySourceCodes.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-row v-for="(r, rowIdx) in filteredNodeArrayForDisplay" :key="rowIdx">
+      <v-col v-for="(cell, cellIdx) in r" :key="cellIdx">
+        <display-source-code
+          :title="cell.title"
+          :sourceCode="cell.sourceCode"
+          :onCopy="copied"
+          class="w-1/2" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import DisplaySourceCode from './DisplaySourceCode.vue'
+import { TreeNode } from '../types/TreeNode'
+
+export default Vue.extend({
+  components: {
+    DisplaySourceCode
+  },
+  props: {
+    searchWord: String
+  },
+  computed: {
+    filteredNodeArrayForDisplay(): TreeNode[][] {
+      const COL_LENGTH_EVERY_ROW = 2
+      const nodeArray = this.$store.getters
+        .filteredNodeArray(this.searchWord) as TreeNode[]
+      const res: TreeNode[][] = []
+      const rowLength =
+        (nodeArray.length + COL_LENGTH_EVERY_ROW - 1) / COL_LENGTH_EVERY_ROW
+      for (let i = 0; i < rowLength; i++) {
+        const cur: TreeNode[] = []
+        for (let j = 0; j < COL_LENGTH_EVERY_ROW &&
+                        i * COL_LENGTH_EVERY_ROW + j < nodeArray.length; j++) {
+          cur.push(nodeArray[i * COL_LENGTH_EVERY_ROW + j])
+        }
+        res.push(cur)
+      }
+
+      return res
+    }
+  },
+  methods: {
+    copied(success: boolean) {
+
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
