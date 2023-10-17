9103f6edff25284e155928b9dd0e6029cef00b6b
sekiya9311 <sekiya9311@gmail.com>
2023-10-18 02:03:06 +0900

    chore: add prettier

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 const withMDX = require('@next/mdx')({
-    options: {
-        remarkPlugins: [],
-        rehypePlugins: [],
-    }
-})
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-    reactStrictMode: true,
-}
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  reactStrictMode: true,
+};
 
-module.exports = withMDX(nextConfig)
+module.exports = withMDX(nextConfig);
